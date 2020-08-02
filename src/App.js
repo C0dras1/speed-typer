5c2b253eda829a07ba16c81fe5c3a87d50b0c58b
Jesse <jessesuderman@gmail.com>
2020-08-02 14:30:47 -0700

    adding words state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  const [ words, setWords] = useState("")
+
+  const handleChange = e => setWords(e.target.value)
+
+  console.log(words)
   return (
-    <div className="App">
+    <div>
+      <h1>Speed Typer!</h1>
+      <textarea
+        name="words" 
+        value={words}
+        onChange={handleChange}
+      />
+      <h4>Time Remaining: 5</h4>
+      <button>Start</button>
+      <h1>Word Count: {words}</h1>
     </div>
   );
 }
