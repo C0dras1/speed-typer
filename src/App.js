0a5ff98cf2b66f0e1ea9d7f6a20810505485cb88
Jesse <jessesuderman@gmail.com>
2020-08-03 14:58:20 -0700

    focusing on textarea fix

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 function App() {
+  const STARTING_TIME = 5
+  
   const [text, setText] = useState("")
-  const [timeRemaining, setTimeRemaining] = useState(3)
+  const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME)
   const [isPlaying, setIsPlaying] = useState(false)
-  const [wordCount, setWordCount] = useState()
+  const [wordCount, setWordCount] = useState(0)
+
+  const textareaRef = useRef(null)
 
   const handleChange = e => setText(e.target.value)
 
 
   function calculateWordCount (text) {
     const wordsArr = text.trim().split(' ')
-    setWordCount(wordsArr.filter(word => word !== "").length)
+    return wordsArr.filter(word => word !== "").length
+  }
+
+  function startGame() {
+    setTimeRemaining(STARTING_TIME)
+    setIsPlaying(true)
+    setText('')
+    textareaRef.current.disabled = false
+    textareaRef.current.focus()
+  }
+
+  function endGame() {
+    setWordCount(calculateWordCount(text))
+    setIsPlaying(false)
   }
 
   useEffect(() => {
@@ -20,8 +37,7 @@ function App() {
         setTimeRemaining(prevTime => prevTime - 1)
       }, 1000)
     } else if (timeRemaining === 0) {
-      calculateWordCount(text)
-      setIsPlaying(false)
+      endGame()
     }
   }, [timeRemaining, isPlaying])
 
@@ -31,9 +47,16 @@ function App() {
       <textarea
         value={text}
         onChange={handleChange}
+        disabled={!isPlaying}
+        ref={textareaRef}
       />
       <h4>Time Remaining: {timeRemaining}</h4>
-      <button onClick={() => setIsPlaying(true)}>Start</button>
+      <button 
+        onClick={startGame} 
+        disabled={isPlaying}
+      >
+        Start
+      </button>
       <h1>Word Count: {wordCount}</h1>
     </div>
   );
