2754debd75d955501a950e2105c9d63956b60789
Jesse <jessesuderman@gmail.com>
2020-08-02 22:38:09 -0700

    MVP

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [ words, setWords] = useState("")
+  const [text, setText] = useState("")
+  const [timeRemaining, setTimeRemaining] = useState(3)
+  const [isPlaying, setIsPlaying] = useState(false)
+  const [wordCount, setWordCount] = useState()
 
-  const handleChange = e => setWords(e.target.value)
+  const handleChange = e => setText(e.target.value)
 
-  console.log(words)
+
+  function calculateWordCount (text) {
+    const wordsArr = text.trim().split(' ')
+    setWordCount(wordsArr.filter(word => word !== "").length)
+  }
+
+  useEffect(() => {
+    if (timeRemaining > 0 && isPlaying) {
+      setTimeout(() => {
+        setTimeRemaining(prevTime => prevTime - 1)
+      }, 1000)
+    } else if (timeRemaining === 0) {
+      calculateWordCount(text)
+      setIsPlaying(false)
+    }
+  }, [timeRemaining, isPlaying])
+
   return (
     <div>
       <h1>Speed Typer!</h1>
       <textarea
-        name="words" 
-        value={words}
+        value={text}
         onChange={handleChange}
       />
-      <h4>Time Remaining: 5</h4>
-      <button>Start</button>
-      <h1>Word Count: {words}</h1>
+      <h4>Time Remaining: {timeRemaining}</h4>
+      <button onClick={() => setIsPlaying(true)}>Start</button>
+      <h1>Word Count: {wordCount}</h1>
     </div>
   );
 }
